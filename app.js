09ba9306f11f4b82c91d142b5c3c3b68dd3b738c
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-26 20:49:43 -0300

    feat: user model and using passport

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
+const bodyParser = require('body-parser');
+
+const User = require('./models/user');
+
+mongoose.connect('mongodb://localhost:27017/auth-demo', {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+});
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('express-session')({
+    secret: 'Kyra is the best and cutest dog',
+    resave: false,
+    saveUninitialized: false
+}));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
     res.render('home');
