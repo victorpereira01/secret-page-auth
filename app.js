8bac3c9f9e3b11eba314389ff22b68ef7e0b8704
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-26 21:06:09 -0300

    feat: registration completed

--- app.js
+++ app.js
@@ -8,23 +8,31 @@ const bodyParser = require('body-parser');
 const User = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/auth-demo', {
-    useNewUrlParser:true,
+    useNewUrlParser: true,
     useUnifiedTopology: true
-});
+})
 
 const app = express();
 
 app.set('view engine', 'ejs');
+
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(require('express-session')({
     secret: 'Kyra is the best and cutest dog',
     resave: false,
     saveUninitialized: false
-}));
+}))
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// =========
+// -> ROUTES 
+// =========
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -32,6 +40,29 @@ app.get('/', (req, res) => {
 
 app.get('/secret', (req, res) => {
     res.render('secret');
+})
+
+//Auth Routes 
+
+//show sign up form
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+//handling user sign up
+app.post('/register', (req, res) => {
+    User.register(new User({
+        username: req.body.username
+    }), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secret');
+            })
+        }
+    })
 })
 
 app.listen(3001);
